6fbd4d12692c809ee61767439678b6ac2d1e3a0a
Abel Yoshuara <abelyoshuara@gmail.com>
2023-12-11 11:27:32 +0800

    add auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PictureController;
 use Illuminate\Http\Request;
@@ -16,9 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Auth without middleware
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
-Route::apiResource('contacts', ContactController::class);
-Route::get('/pictures/{file}', [PictureController::class, 'index']);
+Route::group(['middleware' => 'auth:api'], function () {
+    // Auth with middleware
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+    });
+
+    // Contacts
+    Route::apiResource('contacts', ContactController::class);
+    Route::get('/pictures/{file}', [PictureController::class, 'index']);
+});
