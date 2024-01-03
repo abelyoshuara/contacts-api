54c469e1838cbee3835fe00d4655619266ea4601
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-03 23:27:24 +0800

    add auth features

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PictureController;
 use Illuminate\Http\Request;
@@ -16,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
 
-Route::apiResource('contacts', ContactController::class);
-Route::get('/pictures/{file}', [PictureController::class, 'index']);
+Route::middleware('auth:api')->group(function ($router) {
+    Route::apiResource('contacts', ContactController::class);
+    Route::get('/pictures/{file}', [PictureController::class, 'index']);
+});
